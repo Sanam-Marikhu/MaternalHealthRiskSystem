27a3eefa2bca0ff5e0590044b888660ceca12651
dishan99 <dishanshrestha868@gmail.com>
2026-06-07 13:58:09 +0545

    Initial commit

--- train_model.py
+++ train_model.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import DecisionTreeClassifier
@@ -26,19 +27,45 @@ model.fit(X_train, y_train)
 print("Accuracy:", model.score(X_test, y_test))
 
 # Save model
-import joblib
+
 joblib.dump(model, "model.pkl")
 joblib.dump(encoder, "encoder.pkl")
+print("Model saved successfully!")
 
 # Simple prediction demo
-print("\nPrediction Demo:")
+# print("\nPrediction Demo:")
 
-sample1 = [[25, 120, 80, 6.0, 98.0, 80]]
-prediction = model.predict(sample1)
+# sample1 = [[25, 120, 80, 6.0, 98.0, 80]]
+# prediction = model.predict(sample1)
 
-print("Predicted Risk:", encoder.inverse_transform(prediction)[0])
+# print("Predicted Risk:", encoder.inverse_transform(prediction)[0])
 
-sample2 = [[25, 140, 100, 8.0, 98.0, 80]]
-prediction = model.predict(sample2)
+# sample2 = [[25, 140, 100, 8.0, 98.0, 80]]
+# prediction = model.predict(sample2)
 
-print("Predicted Risk:", encoder.inverse_transform(prediction)[0])
+# print("Predicted Risk:", encoder.inverse_transform(prediction)[0])
+print("\n===== Maternal Health Risk Prediction =====")
+
+age = int(input("Enter Age: "))
+systolic = int(input("Enter Systolic BP: "))
+diastolic = int(input("Enter Diastolic BP: "))
+bs = float(input("Enter Blood Sugar Level: "))
+temp = float(input("Enter Body Temperature: "))
+heart_rate = int(input("Enter Heart Rate: "))
+
+# Create input data
+custom_data = pd.DataFrame([{
+    'Age': age,
+    'SystolicBP': systolic,
+    'DiastolicBP': diastolic,
+    'BS': bs,
+    'BodyTemp': temp,
+    'HeartRate': heart_rate
+}])
+# Predict
+prediction = model.predict(custom_data)
+
+# Convert prediction back to label
+risk_level = encoder.inverse_transform(prediction)
+
+print("\nPredicted Risk Level:", risk_level[0]) 
